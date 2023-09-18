2cfad2371d147d6ce396dff2ce03d2c9977d7e7f
KARAN KUMAR <kkvmalhi@gmail.com>
2023-09-18 22:03:13 +0500

    Environment set

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import express from 'express';
+import path from 'path';
+import authRouter from './routes/auth.mjs'
+import 'dotenv/config' //Environment variable (process.env)
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken' // To verify JW token 
+
+
+
+const __dirname = path.resolve();
+const app = express();
+app.use(express.json()); // body parser
+app.use(cookieParser()) // cookie parser    
+
+
+app.use('/api/v1', authRouter);
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    console.log("req.cookie: ", req.cookies);
+
+    const token = req.cookies.token;
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(decoded);
+        req.body.decoded = {
+            firstName: decoded.firstName,
+            lastName: decoded.lastName,
+            email: decoded.email,
+            isAdmin: decoded.isAdmin
+        }
+        next();
+    } catch (error) {
+        res.status(401).send({
+            message: 'Invalid Token'
+        });
+    }
+})
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`App is Running On Port: ${PORT}`);
+
+})
---
+++ router/auth.mjs
@@ -0,0 +1,111 @@
+import express from 'express';
+let router = express.Router();
+import jwt from 'jsonwebtoken' 
+import { stringToHash, varifyHash } from 'bcrypt-inzi' 
+import { client } from '../mongoDb.mjs'
+const userCollection = client.db('OIBSIP-Auth').collection("Users");
+
+router.post('/login', async (req, res, next) => {
+    if (
+        !req.body?.username
+        || !req.body.password
+    ) {
+        res.status(403).send(
+            `Required Parameters Missing
+                Please Fill All the Fields
+                email : "abc@gmail.com"
+                Password: "Password"`
+        );
+        return;
+    }
+
+    req.body.username = req.body.username.toLowerCase();
+
+    try {
+        let result = await userCollection.findOne({ username: req.body.username });
+        console.log(result);
+
+        if (!result) {  
+            res.status(403).send({
+                message: "Username & Password Incorrect"
+            });
+            return;
+        } else {
+            const isMatch = await varifyHash(req.body.password, result.password) 
+            if (isMatch) { 
+                const token = jwt.sign({  
+                    isAdmin: false,
+                    username: req.body.username,
+                    email: req.body.email,
+                }, process.env.SECRET_KEY, {
+                    expiresIn: '1h' 
+                });  
+                res.cookie('token', token, {
+                    httpOnly: true, 
+                    secure: true,
+                })
+                res.send({
+                    message: "Login Successfully",
+                });
+            } else {
+                res.status(403).send({
+                    message: "Email & Password Incorrect"
+                });
+                return;
+            }
+        }
+    } catch (err) {
+        console.log(`Error:${err}`);
+        res.status(500).send('Server Error Please try Again Later');
+    }
+});
+
+
+
+
+router.post('/signup', async (req, res, next) => {
+    if ( 
+        !req.body?.username
+        || !req.body?.password
+    ) {
+        res.status(403).send(
+            `Required Parameters Missing
+                Please Fill All the Fields
+                
+                username : "xyz",
+                email : "abc@gmail.com",
+                Password: "Password"`
+        );
+        return;
+    }
+
+    req.body.username = req.body.username.toLowerCase(); 
+
+    try {
+        let result = await userCollection.findOne({ username: req.body.username }); 
+        console.log(result);
+
+        if (!result) {
+            const passwordHash = await stringToHash(req.body.password) 
+            const insertResponse = await userCollection.insertOne({
+                username: req.body.username,
+                email: req.body.email,
+                password: passwordHash,
+                CreatedOn: new Date
+            });
+            console.log(`Insert Response: ${insertResponse}`);
+            res.send({
+                message: "Signup Successfully"
+            })
+        } else {
+            res.status(403).send({
+                message: "The Email is Already Registered"
+            });
+        }
+    } catch (err) {
+        console.log(`Error:${err}`);
+        res.status(500).send('Server Error Please try Again Later');
+    }
+});
+
+export default router;
